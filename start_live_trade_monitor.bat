e3ceb57602b02cfb2cc9292a53ce9c137b77e9ec
somu-analyst <srinivas.analystsas@gmail.com>
2026-04-24 08:17:57 -0400

    24apr_v1 change

---
+++ start_live_trade_monitor.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d c:\Users\srini\Options_chain_data\NYSE_DATA
+C:/Users/srini/AppData/Local/Microsoft/WindowsApps/python3.13.exe c:/Users/srini/Options_chain_data/NYSE_DATA/start_live_trade_monitor.py start --interval 15
